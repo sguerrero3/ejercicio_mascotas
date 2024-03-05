db73f5803c4ae6c60ae8908be3a6c011d442866d
sguerrero3 <s.guerrero3@uniandes.edu.co>
2024-03-05 10:03:08 -0500

    ahora si

--- src/mascotas.js
+++ src/mascotas.js
@@ -1,28 +1,34 @@
 import Mascota from "./mascota";
-import { useEffect, useState } from "react";
-import Row from 'react-bootstrap/Row';
+import Row from "react-bootstrap/Row";
 import Col from "react-bootstrap/Col";
 
-function Mascotas () {
+const { useEffect, useState } = require("react");
 
-    const [mascotas, setMascotas] = useState([]);
+function Mascotas() {
+ const [mascotas, setMascotas] = useState([]);
+ useEffect(() => {
+   const URL =
+     "https://gist.githubusercontent.com/josejbocanegra/829a853c6c68880477697acd0490cecc/raw/99c31372b4d419a855e53f0e891246f313a71b20/mascotas.json";
+   fetch(URL)
+     .then((data) => data.json())
+     .then((data) => {
+       setMascotas(data);
+     });
+ }, []);
 
-    useEffect(()=> {
-        const URL = "https://gist.githubusercontent.com/josejbocanegra/829a853c6c68880477697acd0490cecc/raw/99c31372b4d419a855e53f0e891246f313a71b20/mascotas.json";
-
-        fetch(URL).then(data => data.json()).then(data => {setMascotas(data);})
-    }, []);
-
-    return(
-        <div className="container">
-            <h1>Listado de mascotas</h1>
-            <hr></hr>
-            <Row>
-                {mascotas.map(mascota => <Col><Mascota mascota={mascota} key={mascota.id}/></Col>)}               
-            </Row>
-           
-        </div>
-    )
+ return (
+   <div className="container">
+     <h2 className="mt-2">Listado de mascotas</h2>
+     <hr></hr>
+     <Row>
+       {mascotas.map((mascota) => (
+         <Col key={mascota.id}>
+           <Mascota mascota={mascota} />
+         </Col>
+       ))}
+     </Row>
+   </div>
+ );
 }
 
 export default Mascotas;
